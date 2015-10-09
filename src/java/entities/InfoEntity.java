d1b7475d50b1a4494487ff5efec51f4eed077cb4
martinweberhansen <martinweberhansen@gmail.com>
2015-10-09 10:59:22 +0200

    rest & facade virker..

--- src/java/entities/InfoEntity.java
+++ src/java/entities/InfoEntity.java
@@ -3,25 +3,69 @@
 package entities;
 
 import java.io.Serializable;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import static javax.persistence.CascadeType.ALL;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @Entity
-
 @Inheritance(strategy = InheritanceType.JOINED)
 
 public class InfoEntity implements Serializable {
-
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String email;
 
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private Address address;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+    
     public InfoEntity(int id, String email) {
         this.id = id;
         this.email = email;
